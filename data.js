9562a784e1dcf66f9f8ab4f818c53326c08ccab2
anh tien <anhtienb237@gmail.com>
2024-05-11 01:08:20 +0700

    ok

---
+++ data.js
@@ -0,0 +1,22 @@
+export default imageData = {
+    cable_car: {
+        name: "Cable Car",
+        pdf: "PDF doanload file/cable car02.pdf",
+        image: "PDF doanload file/Screenshot 2023-11-02 165352.png"
+    },
+    racing_car: {
+        name: "Racing Car",
+        pdf: "PDF doanload file/racing car.pdf",
+        image: "PDF doanload file/cable car02 .io",
+    },
+    santa: {
+        name: "Santa Claus Sleigh",
+        pdf: "PDF doanload file/Untitled Model.pdf",
+        image: "PDF doanload file/Screenshot 2023-12-09 002240.png",
+    },
+    sumo_walking:{
+        name : "Sumo Walking",
+        pdf : "PDF doanload file/Sumo_Walking.pdf",
+        imgage : "PDF doanload file/Sumo_Walking.png"
+    }
+}
